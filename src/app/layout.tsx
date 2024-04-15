6d16c59f909c1244cf8c0a778b7984123104523b
gastawny <gastawny@gmail.com>
2024-04-14 23:45:35 -0300

    feat: Create setup with Next.js #T3P-45

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'T3-Web',
+  description: 'T3-Web',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
